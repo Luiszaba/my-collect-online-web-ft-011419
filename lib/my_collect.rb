d3f44dc9550b0f7e3190127e0ced9d7c6f3901f1
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 18:41:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array)
- array = []
+ spoken_languages = []
  counter=0
    while counter < languages.length
-   spoken_languages << yield (languages[i])
+   spoken_languages << yield(languages[i])
    counter +=1
  end
    spoken_languages
