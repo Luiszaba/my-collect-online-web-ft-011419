3bfd7345554eb70d689067fc28209bbe5d42a10e
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 18:36:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(languages)
+ languages=["1","2","3"]
+ i=0
+ my_collect(languages) do |language|
+   while i < language.length
+   i=i+1
+   spoken_languages << yield(language[i])
+ end
+   spoken_languages
+ end
+ end
